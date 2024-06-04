8ccc9a2984ec3c5f2fa61d6f087cc3338e19067d
JW0203 <widwoo0203@gmail.com>
2024-06-04 11:39:47 +0900

    feat: install and set typeORM for local env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'local', 'debug').default('local'),
+      }),
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_HOST'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
