802d1217b39df63e3f7412c7074025ba872aadba
JW0203 <widwoo0203@gmail.com>
2024-06-04 11:45:28 +0900

    add initial setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  // app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(configService.get<number>('PORT'), configService.get<string>('HOST_IP'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
