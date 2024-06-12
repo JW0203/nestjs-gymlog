1a2ea114f6d07c245a7f5f3027552de6a9551dfe
JW0203 <widwoo0203@gmail.com>
2024-06-12 14:56:14 +0900

    fix: fix the entity realtions

--- src/routine/domain/Routine.entity.ts
+++ src/routine/domain/Routine.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { BodyPart } from '../../excercise/domain/bodyPart.enum';
 import { Timestamps } from '../../TimeStamp.entity';
+import { User } from '../../user/domain/User.entity';
 
 @Entity()
 export class Routine extends Timestamps {
@@ -15,6 +16,9 @@ export class Routine extends Timestamps {
 
   @Column()
   exerciseName: string;
+
+  @ManyToOne(() => User, (user) => user.routines)
+  public user: User;
 
   constructor();
   constructor(params: { name: string; bodyPart: BodyPart; exerciseName: string });
