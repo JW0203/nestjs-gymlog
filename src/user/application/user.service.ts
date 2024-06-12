4320cf3c84e74e08cbf4f5a4ff2f9ed639ed5dec
JW0203 <widwoo0203@gmail.com>
2024-06-12 23:34:26 +0900

    feat: hashing the password

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../domain/User.entity';
 import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {}
 
   async signUp(user: User): Promise<User> {
-    const newUser = new User(user);
+    const { password, ...results } = user;
+    const hashingPassword: string = await bcrypt.hash(
+      password,
+      this.configService.get<number>('SALT_ROUNDS') as number,
+    );
+    const newUser = new User({ ...results, password: hashingPassword });
     return await this.userRepository.save(newUser);
   }
 }
