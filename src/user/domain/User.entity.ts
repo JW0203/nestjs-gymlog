d9e2db96f9521b157224b2510adec429b84a23bf
JW0203 <widwoo0203@gmail.com>
2024-06-12 14:55:40 +0900

    fix: fix the entity realtions

--- src/user/domain/User.entity.ts
+++ src/user/domain/User.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { WorkoutLog } from '../../workoutLog/domain/WorkoutLog.entity';
+import { Routine } from '../../routine/domain/Routine.entity';
 
 @Entity()
 export class User extends Timestamps {
@@ -16,8 +17,11 @@ export class User extends Timestamps {
   @Column()
   name: string;
 
-  @OneToMany(() => WorkoutLog, (workoutLog) => workoutLog.id)
-  public workoutLog: WorkoutLog[];
+  @OneToMany(() => WorkoutLog, (workoutLog) => workoutLog.user)
+  public workoutLogs: WorkoutLog[];
+
+  @OneToMany(() => Routine, (routine) => routine.user)
+  public routines: Routine[];
 
   constructor(params: { email: string; password: string; name: string }) {
     super();
