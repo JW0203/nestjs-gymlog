5c6ddae3a2b7be2d3966d9783b0c39abb64975a0
JW0203 <widwoo0203@gmail.com>
2024-06-13 11:31:16 +0900

    feat: implement API to issue JWT tokens

---
+++ src/auth/application/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRESIN') },
+      }),
+    }),
+  ],
+})
+export class AuthModule {}
