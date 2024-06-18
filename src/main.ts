de971e44bb816ec0709cf4c1880788ecd3798a3b
JW0203 <widwoo0203@gmail.com>
2024-06-18 21:38:03 +0900

    chore: install libraries for validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT') as string;
   const hostIP = configService.get<string>('HOST_IP') as string;
