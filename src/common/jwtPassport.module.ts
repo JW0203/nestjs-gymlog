81daca9031eb153be22f8544df1551f7a92b8948
JW0203 <widwoo0203@gmail.com>
2024-06-18 16:31:18 +0900

    feat: implement JWT token generation

--- src/common/jwtPassport.module.ts
+++ src/common/jwtPassport.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UserModule } from '../../user/application/user.module';
-import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../user/user.module';
+import { JwtStrategy } from './jwtPassport/jwt.strategy';
 
 @Module({
   imports: [PassportModule, UserModule],
