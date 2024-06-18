ec107c01b1f26ae6b2c64f31dbb3ba19fdbc63bb
JW0203 <widwoo0203@gmail.com>
2024-06-19 00:23:28 +0900

    feat: apply email validations

--- src/user/domain/User.entity.ts
+++ src/user/domain/User.entity.ts
@@ -2,12 +2,16 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { WorkoutLog } from '../../workoutLog/domain/WorkoutLog.entity';
 import { Routine } from '../../routine/domain/Routine.entity';
+import { IsNotEmpty } from 'class-validator';
+import { IsEmailCustom } from '../../common/isEmailValidation.custom';
 
 @Entity()
 export class User extends Timestamps {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
+  @IsEmailCustom()
   @Column()
   email: string;
 
