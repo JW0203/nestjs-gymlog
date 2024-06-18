fc36cf37ef6ae1a0eb27d408928185c7944459ae
JW0203 <widwoo0203@gmail.com>
2024-06-19 00:48:32 +0900

    fix: add decorators to check validation

--- src/user/dto/signUp.request.dto.ts
+++ src/user/dto/signUp.request.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import { NoWhitespace } from '../../common/NoWhitespace.validation';
+import { IsEmailCustom } from '../../common/isEmailValidation.custom';
+
 export class SignUpRequestDto {
+  @IsEmailCustom()
   email: string;
+
+  @IsNotEmpty()
+  @NoWhitespace()
+  @MinLength(8)
+  @MaxLength(15)
   password: string;
+
   name: string;
 }
