22fedc9586d92f5409cee89e342df7a068890e51
JW0203 <widwoo0203@gmail.com>
2024-06-19 22:36:32 +0900

    refactor: remove duplicated columns with excercise

--- src/routine/domain/Routine.entity.ts
+++ src/routine/domain/Routine.entity.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { BodyPart } from '../../excercise/domain/bodyPart.enum';
 import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
 import { RoutineToExercise } from '../../routineToExercise/domain/RoutineToExercise.entity';
@@ -12,12 +11,6 @@ export class Routine extends Timestamps {
   @Column()
   name: string;
 
-  @Column({ type: 'enum', enum: BodyPart })
-  bodyPart: BodyPart;
-
-  @Column()
-  exerciseName: string;
-
   @ManyToOne(() => User, (user) => user.routines)
   public user: User;
 
@@ -25,13 +18,11 @@ export class Routine extends Timestamps {
   public routineToExercises: RoutineToExercise[];
 
   constructor();
-  constructor(params: { name: string; bodyPart: BodyPart; exerciseName: string });
-  constructor(params?: { name: string; bodyPart: BodyPart; exerciseName: string }) {
+  constructor(params: { name: string });
+  constructor(params?: { name: string }) {
     super();
     if (params) {
       this.name = params.name;
-      this.bodyPart = params.bodyPart;
-      this.exerciseName = params.exerciseName;
     }
   }
 }
