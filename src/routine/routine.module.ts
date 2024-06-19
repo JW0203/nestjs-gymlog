c828ccd9552e2b36632ea71f35e58a5fc15ab33b
JW0203 <widwoo0203@gmail.com>
2024-06-19 22:37:17 +0900

    feat: add routineToExercise module

--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './domain/Routine.entity';
+import { RoutineService } from './application/routine.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Routine])],
+  providers: [RoutineService],
 })
 export class RoutineModule {}
