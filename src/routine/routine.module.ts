21580494115af2a11df738433fd7faaa98c570bc
JW0203 <widwoo0203@gmail.com>
2024-06-20 21:53:34 +0900

    feat: import files for implement controller and service

--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './domain/Routine.entity';
 import { RoutineService } from './application/routine.service';
+import { ExerciseModule } from '../excercise/excercise.module';
+import { RoutineToExerciseModule } from '../routineToExercise/routineToExercise.module';
+import { UserModule } from '../user/user.module';
+import { RoutineController } from './presentation/routine.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Routine])],
+  imports: [TypeOrmModule.forFeature([Routine]), ExerciseModule, RoutineToExerciseModule, UserModule],
   providers: [RoutineService],
+  controllers: [RoutineController],
 })
 export class RoutineModule {}
