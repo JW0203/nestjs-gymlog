65187b40914d79055f0587248c42def77076fe49
JW0203 <widwoo0203@gmail.com>
2024-06-20 21:51:04 +0900

    feat: create DTO for saveRoutine method to handle routine data requests

--- src/routine/dto/saveRoutine.request.dto.ts
+++ src/routine/dto/saveRoutine.request.dto.ts
@@ -2,7 +2,6 @@ import { BodyPart } from '../../excercise/domain/bodyPart.enum';
 
 export class SaveRoutineRequestDto {
   routineName: string;
-  userId: number;
   exerciseName: string;
   bodyPart: BodyPart;
 }
