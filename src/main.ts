1ff0743a8516a12a5c301d5ed19dac0af1b1b564
JW0203 <widwoo0203@gmail.com>
2024-06-26 17:44:03 +0900

    feat: setup logger service

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { LoggerService } from './common/Logger/logger.service';
 
 async function bootstrap() {
   initializeTransactionalContext();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(LoggerService));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
