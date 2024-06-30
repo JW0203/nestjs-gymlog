5b272f657ba01a4fcedb59841bfe5e27747601d1
JW0203 <widwoo0203@gmail.com>
2024-06-30 18:13:25 +0900

    feat: implement update method

--- src/workoutLogToExercise/application/workoutLogToExercise.service.ts
+++ src/workoutLogToExercise/application/workoutLogToExercise.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { WorkoutLogToExercise } from '../domain/WorkoutLogToExercise.entity';
 import { SaveWorkoutLogToExerciseRequestDto } from '../dto/saveWorkoutLogToExercise.request.dto';
+import { UpdateWorkoutLogToExerciseRequestDto } from '../dto/updateWorkoutLogToExercise.request.dto';
 
 @Injectable()
 export class WorkoutLogToExerciseService {
+  private readonly logger = new Logger(WorkoutLogToExerciseService.name);
   constructor(
     @InjectRepository(WorkoutLogToExercise)
     private readonly workoutLogToExerciseRepository: Repository<WorkoutLogToExercise>,
@@ -13,5 +15,14 @@ export class WorkoutLogToExerciseService {
 
   async saveWorkoutLogToExercise(saveWorkoutLogToExerciseRequestDto: SaveWorkoutLogToExerciseRequestDto) {
     return await this.workoutLogToExerciseRepository.save(saveWorkoutLogToExerciseRequestDto);
+  }
+
+  async updateWorkoutLogToExercise(
+    id: number,
+    updateWorkoutLogToExerciseRequestDto: UpdateWorkoutLogToExerciseRequestDto,
+  ) {
+    this.logger.error(`Database update starts`);
+    const updated = await this.workoutLogToExerciseRepository.update(id, updateWorkoutLogToExerciseRequestDto);
+    return updated.affected ? 'updated' : false;
   }
 }
