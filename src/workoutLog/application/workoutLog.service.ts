95a8ccc09d2fecc5d0cd6dd7be3e6afd469a1278
JW0203 <widwoo0203@gmail.com>
2024-07-02 21:49:36 +0900

    refactor: remove unused method

--- src/workoutLog/application/workoutLog.service.ts
+++ src/workoutLog/application/workoutLog.service.ts
@@ -16,34 +16,6 @@ export class WorkoutLogService {
     private readonly userService: UserService,
   ) {}
 
-  // @Transactional()
-  // async saveWorkoutLogs(
-  //   userId: number,
-  //   saveWorkoutLogRequestDtoArray: SaveWorkoutLogRequestDto[],
-  // ): Promise<WorkoutLog[]> {
-  //   const user = await this.userService.findOneById(userId);
-  //   if (!user) {
-  //     throw new NotFoundException('User not found');
-  //   }
-  //   const savedWorkoutLog = [];
-  //   //Todo: promise.all 로 전환
-  //   for (const saveWorkoutLogRequestDto of saveWorkoutLogRequestDtoArray) {
-  //     const { exerciseName, bodyPart, set, weight, repeat } = saveWorkoutLogRequestDto;
-  //     const workoutLog = new WorkoutLog({ set, weight, repeat });
-  //     workoutLog.user = user;
-  //     let exercise = await this.exerciseService.findByExerciseNameAndBodyPart({ exerciseName, bodyPart });
-  //     if (!exercise) {
-  //       exercise = await this.exerciseService.saveExercise({ exerciseName, bodyPart });
-  //     }
-  //     workoutLog.exercise = exercise;
-  //     // Todo: 반환된 값에서 유저는 이름과 아이디만
-  //     const newWorkoutLog = await this.workoutLogRepository.save(workoutLog);
-  //     savedWorkoutLog.push(newWorkoutLog);
-  //   }
-  //
-  //   return savedWorkoutLog;
-  // }
-
   @Transactional()
   async saveWorkoutLogs(userId: number, saveWorkoutLogRequestDtoArray: SaveWorkoutLogRequestDto[]): Promise<any[]> {
     const user = await this.userService.findOneById(userId);
