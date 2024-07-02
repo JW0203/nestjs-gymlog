c4d25e1474b7dc9598104bbf669212e5da9467ff
JW0203 <widwoo0203@gmail.com>
2024-07-02 15:33:37 +0900

    feat: setup workoutLog module

--- src/workoutLog/workoutLog.module.ts
+++ src/workoutLog/workoutLog.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkoutLog } from './domain/WorkoutLog.entity';
+import { ExerciseModule } from '../excercise/excercise.module';
+import { WorkoutLogController } from './presentation/workoutLog.controller';
+import { UserModule } from '../user/user.module';
+import { WorkoutLogService } from './application/workoutLog.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WorkoutLog])],
+  imports: [TypeOrmModule.forFeature([WorkoutLog]), ExerciseModule, UserModule],
+  controllers: [WorkoutLogController],
+  providers: [WorkoutLogService],
 })
 export class WorkoutLogModule {}
