8e7e3354073ba2f676722dc411391ce37a074c9f
JW0203 <widwoo0203@gmail.com>
2024-07-02 15:30:36 +0900

    feat: make function to handle response data from save workoutLog

---
+++ src/workoutLog/application/functions/reponseFormatWorkoutLog.ts
@@ -0,0 +1,20 @@
+import { WorkoutLog } from '../../domain/WorkoutLog.entity';
+import { User } from '../../../user/domain/User.entity';
+import { Exercise } from '../../../excercise/domain/Exercise.entity';
+
+function reponseFormatWorkoutLog(workoutLog: WorkoutLog, user: User, exercise: Exercise): any {
+  return {
+    ...workoutLog,
+    user: {
+      id: user.id,
+      name: user.name,
+    },
+    exercise: {
+      id: exercise.id,
+      exerciseName: exercise.exerciseName,
+      bodyPart: exercise.bodyPart,
+    },
+  };
+}
+
+export { reponseFormatWorkoutLog };
