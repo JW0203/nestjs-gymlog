908e936b34d4de86a9ee234a8895a00b376b6750
JW0203 <widwoo0203@gmail.com>
2024-07-04 16:01:06 +0900

    refactor: add validators

--- src/workoutLog/dto/SaveWorkoutLog.request.dto.ts
+++ src/workoutLog/dto/SaveWorkoutLog.request.dto.ts
@@ -1,9 +1,21 @@
 import { BodyPart } from '../../excercise/domain/bodyPart.enum';
+import { IsEnum, IsInt, Min } from 'class-validator';
 
 export class SaveWorkoutLogRequestDto {
   exerciseName: string;
+
+  @IsEnum(BodyPart)
   bodyPart: BodyPart;
+
+  @IsInt()
+  @Min(1)
   set: number;
+
+  @IsInt()
+  @Min(1)
   weight: number;
+
+  @IsInt()
+  @Min(1)
   repeat: number;
 }
