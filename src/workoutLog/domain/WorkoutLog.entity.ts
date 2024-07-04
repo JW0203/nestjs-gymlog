b35b0148fa31bded4b1ea093223b5e051c0707a0
JW0203 <widwoo0203@gmail.com>
2024-07-04 14:46:40 +0900

    feat: add IsNotEmpty decorator

--- src/workoutLog/domain/WorkoutLog.entity.ts
+++ src/workoutLog/domain/WorkoutLog.entity.ts
@@ -2,18 +2,22 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
 import { Exercise } from '../../excercise/domain/Exercise.entity';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class WorkoutLog extends Timestamps {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
   @Column()
   set: number;
 
+  @IsNotEmpty()
   @Column()
   weight: number;
 
+  @IsNotEmpty()
   @Column()
   repeat: number;
 
