29028aab3516659a3e8c3d5215f2fc5437604f61
JW0203 <widwoo0203@gmail.com>
2024-07-09 10:47:40 +0900

    feat: implement bluk update api

--- src/workoutLog/presentation/workoutLog.controller.ts
+++ src/workoutLog/presentation/workoutLog.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, UseGuards, Request, Query, Get } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Query, Get, Patch } from '@nestjs/common';
 import { WorkoutLogService } from '../application/workoutLog.service';
 import { SaveWorkoutLogRequestDto } from '../dto/SaveWorkoutLog.request.dto';
 import { JwtAuthGuard } from '../../common/jwtPassport/jwtAuth.guard';
 import { ExerciseDataRequestDto } from '../dto/exerciseData.request.dto';
+import { UpdateWorkoutLogRequestDto } from '../dto/updateWorkoutLog.request.dto';
 
 @Controller('workout-logs')
 export class WorkoutLogController {
@@ -22,5 +23,15 @@ export class WorkoutLogController {
   @UseGuards(JwtAuthGuard)
   getWorkoutLogs(@Query('date') date: string, @Request() req: any) {
     return this.workoutLogService.getWorkoutLogsByDay(date, req.user.id);
+  }
+
+  @Patch()
+  @UseGuards(JwtAuthGuard)
+  bulkUpdateWorkoutLogs(
+    @Request() req: any,
+    @Body('exercises') exercises: ExerciseDataRequestDto[],
+    @Body('workoutLogs') updateWorkoutLogRequestDtoArray: UpdateWorkoutLogRequestDto[],
+  ) {
+    return this.workoutLogService.bulkUpdateWorkoutLogs(req.user.id, exercises, updateWorkoutLogRequestDtoArray);
   }
 }
