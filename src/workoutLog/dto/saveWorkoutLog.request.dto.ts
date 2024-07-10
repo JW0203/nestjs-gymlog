9288dcee61a44d5bda6ab7bacb7d88296c37e89c
JW0203 <widwoo0203@gmail.com>
2024-07-10 10:45:17 +0900

    refactor: add cross-validations

--- src/workoutLog/dto/saveWorkoutLog.request.dto.ts
+++ src/workoutLog/dto/saveWorkoutLog.request.dto.ts
@@ -1,7 +1,15 @@
 import { BodyPart } from '../../excercise/domain/bodyPart.enum';
-import { IsEnum, IsInt, Min } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsString, Length, Matches, Min } from 'class-validator';
 
 export class SaveWorkoutLogRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 50, {
+    message: 'Exercise name must be between 3 and 50 characters',
+  })
+  @Matches(/^[a-zA-Z0-9 ]*$/, {
+    message: 'Exercise name can only contain letters, numbers, and spaces',
+  })
   exerciseName: string;
 
   @IsEnum(BodyPart)
