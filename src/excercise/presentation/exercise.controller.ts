2e5287af3352b9e742861c85551e18f3cced05e4
JW0203 <widwoo0203@gmail.com>
2024-07-10 22:50:03 +0900

    feat: add post and get APIs

--- src/excercise/presentation/exercise.controller.ts
+++ src/excercise/presentation/exercise.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ExerciseService } from '../application/exercise.service';
+import { SaveExerciseRequestDto } from '../dto/saveExercise.request.dto';
 
 @Controller('exercises')
 export class ExerciseController {
@@ -8,5 +9,20 @@ export class ExerciseController {
   @Delete(':id')
   softDelete(@Param('id') id: string) {
     return this.exerciseService.softDelete(parseInt(id));
+  }
+
+  @Post()
+  save(@Body() saveExerciseRequestDto: SaveExerciseRequestDto[]) {
+    return this.exerciseService.bulkInsertExercises(saveExerciseRequestDto);
+  }
+
+  @Get()
+  get(@Body() saveExerciseRequestDto: SaveExerciseRequestDto) {
+    return this.exerciseService.findByExerciseNameAndBodyPart(saveExerciseRequestDto);
+  }
+
+  @Get('all')
+  getAll(@Body() saveExerciseRequestDto: SaveExerciseRequestDto[]) {
+    return this.exerciseService.findAll(saveExerciseRequestDto);
   }
 }
