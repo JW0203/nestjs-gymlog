a89261c7d1bcfd6c17af9fbb7c821a35047eced6
JW0203 <widwoo0203@gmail.com>
2024-07-14 11:53:01 +0900

    feat: make dto to handle request data for saving exercise

--- src/excercise/dto/saveExercise.request.dto.ts
+++ src/excercise/dto/saveExercise.request.dto.ts
@@ -1,6 +1,20 @@
-import { BodyPart } from '../domain/bodyPart.enum';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+import { IsExerciseName } from '../../common/validation/isExerciseName.validation';
+import { BodyPart } from '../../common/bodyPart.enum';
 
 export class SaveExerciseRequestDto {
+  @IsNotEmpty()
+  @IsExerciseName()
   exerciseName: string;
+
+  @IsNotEmpty()
+  @IsEnum(BodyPart)
   bodyPart: BodyPart;
+
+  constructor(params: { exerciseName: string; bodyPart: BodyPart }) {
+    if (params) {
+      this.exerciseName = params.exerciseName;
+      this.bodyPart = params.bodyPart;
+    }
+  }
 }
