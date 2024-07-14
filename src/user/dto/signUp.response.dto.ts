a46dc74469798cc6cf5b90fbbf84ab07456b8194
JW0203 <widwoo0203@gmail.com>
2024-07-14 12:06:30 +0900

    refactor: add cross-validations

--- src/user/dto/signUp.response.dto.ts
+++ src/user/dto/signUp.response.dto.ts
@@ -1,6 +1,18 @@
+import { IsInt, IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsEmailCustom } from '../../common/validation/isEmail.validation.custom';
+
 export class SignUpResponseDto {
+  @IsNotEmpty()
+  @IsInt()
   id: number;
+
+  @IsEmailCustom()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(15)
+  @Matches(/^[A-Za-z0-9]+$/)
   name: string;
 
   constructor(params: { id: number; email: string; name: string }) {
