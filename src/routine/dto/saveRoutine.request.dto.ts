a1204a47326cdf72ab040b63454a0f94653f62b4
JW0203 <widwoo0203@gmail.com>
2024-07-15 21:32:13 +0900

    refactor: add cross-validator

--- src/routine/dto/saveRoutine.request.dto.ts
+++ src/routine/dto/saveRoutine.request.dto.ts
@@ -1,7 +1,14 @@
-import { BodyPart } from '../../excercise/domain/bodyPart.enum';
+import { BodyPart } from '../../common/bodyPart.enum';
+import { IsEnum, Matches } from 'class-validator';
+import { IsExerciseName } from '../../common/validation/isExerciseName.validation';
 
 export class SaveRoutineRequestDto {
+  @Matches(/^[a-zA-Z0-9\uAC00-\uD7A3\s]*$/)
   routineName: string;
+
+  @IsExerciseName()
   exerciseName: string;
+
+  @IsEnum(BodyPart)
   bodyPart: BodyPart;
 }
