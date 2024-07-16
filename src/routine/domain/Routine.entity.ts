73aedc17a9076fddb551febfc80fe94525c00a97
JW0203 <widwoo0203@gmail.com>
2024-07-16 12:44:12 +0900

    feat: remove routineToExercise

--- src/routine/domain/Routine.entity.ts
+++ src/routine/domain/Routine.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
-import { RoutineToExercise } from '../../routineToExercise/domain/RoutineToExercise.entity';
+import { validateOrReject } from 'class-validator';
+import { Exercise } from '../../excercise/domain/Exercise.entity';
 
 @Entity()
 export class Routine extends Timestamps {
@@ -14,15 +15,20 @@ export class Routine extends Timestamps {
   @ManyToOne(() => User, (user) => user.routines)
   public user: User;
 
-  @OneToMany(() => RoutineToExercise, (routineToExercise) => routineToExercise.routine)
-  public routineToExercises: RoutineToExercise[];
+  @ManyToOne(() => Exercise, (Exercise) => Exercise.routines)
+  public exercise: Exercise;
 
   constructor();
-  constructor(params: { name: string });
-  constructor(params?: { name: string }) {
+  constructor(params: { name: string; user: User });
+  constructor(params?: { name: string; user: User }) {
     super();
     if (params) {
       this.name = params.name;
+      this.user = params.user;
+
+      validateOrReject(this).catch((errors) => {
+        console.log('(Routine entity validation failed). Errors: ', errors);
+      });
     }
   }
 }
--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -3,13 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './domain/Routine.entity';
 import { RoutineService } from './application/routine.service';
 import { ExerciseModule } from '../excercise/excercise.module';
-import { RoutineToExerciseModule } from '../routineToExercise/routineToExercise.module';
 import { UserModule } from '../user/user.module';
 import { RoutineController } from './presentation/routine.controller';
 import { LoggerModule } from '../common/Logger/logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Routine]), ExerciseModule, RoutineToExerciseModule, UserModule, LoggerModule],
+  imports: [TypeOrmModule.forFeature([Routine]), ExerciseModule, UserModule, LoggerModule],
   providers: [RoutineService],
   controllers: [RoutineController],
 })
