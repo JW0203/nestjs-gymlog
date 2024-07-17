dc136dc3780f8758136d50731b58e858590f1388
JW0203 <widwoo0203@gmail.com>
2024-07-17 18:53:21 +0900

    refactor: change the name of dto

--- src/excercise/dto/saveExercises.request.dto.ts
+++ src/excercise/dto/saveExercises.request.dto.ts
@@ -1,9 +1,9 @@
-import { SaveExerciseRequestDto } from './saveExercise.request.dto';
 import { Type } from 'class-transformer';
 import { ValidateNested } from 'class-validator';
+import { ExerciseDataFormatDto } from '../../common/dto/exerciseData.format.dto';
 
 export class ExerciseDataArrayRequestDto {
   @ValidateNested({ each: true })
-  @Type(() => SaveExerciseRequestDto)
-  exercises: SaveExerciseRequestDto[];
+  @Type(() => ExerciseDataFormatDto)
+  exercises: ExerciseDataFormatDto[];
 }
