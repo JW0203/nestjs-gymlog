66fe43197f3b8ebd53f7a06bd05faf068d72018b
JW0203 <widwoo0203@gmail.com>
2024-07-17 23:05:24 +0900

    refactor: apply cross validators

--- src/workoutLog/domain/WorkoutLog.entity.ts
+++ src/workoutLog/domain/WorkoutLog.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
 import { Exercise } from '../../excercise/domain/Exercise.entity';
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, Length, validateOrReject } from 'class-validator';
 
 @Entity()
 export class WorkoutLog extends Timestamps {
@@ -10,14 +10,20 @@ export class WorkoutLog extends Timestamps {
   id: number;
 
   @IsNotEmpty()
+  @IsInt()
+  @Length(1, 100)
   @Column()
   set: number;
 
   @IsNotEmpty()
+  @IsInt()
+  @Length(1, 1000)
   @Column()
   weight: number;
 
   @IsNotEmpty()
+  @IsInt()
+  @Length(1, 1000)
   @Column()
   repeat: number;
 
@@ -27,18 +33,17 @@ export class WorkoutLog extends Timestamps {
   @ManyToOne(() => User, (user) => user.workoutLogs)
   public user: User;
 
-  constructor(params: { set: number; weight: number; repeat: number; exercise?: Exercise; user?: User }) {
+  constructor(params: { set: number; weight: number; repeat: number; exercise: Exercise; user: User }) {
     super();
     if (params) {
       this.set = params.set;
       this.weight = params.weight;
       this.repeat = params.repeat;
-      if (params.exercise) {
-        this.exercise = params.exercise;
-      }
-      if (params.user) {
-        this.user = params.user;
-      }
+      this.exercise = params.exercise;
+      this.user = params.user;
     }
+    validateOrReject(this).catch((errors) => {
+      console.log('(WorkoutLog entity validation failed). Errors: ', errors);
+    });
   }
 }
