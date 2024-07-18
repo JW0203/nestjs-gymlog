09cb690326be45cff5cb7a1cec31cf3b4d0a504d
JW0203 <widwoo0203@gmail.com>
2024-07-19 01:44:39 +0900

    feat: add codes to prevent concurrency issue and duplicated data

--- src/routine/application/routine.service.ts
+++ src/routine/application/routine.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Routine } from '../domain/Routine.entity';
-import { In, Repository } from 'typeorm';
+import { DataSource, In, Repository } from 'typeorm';
 import { ExerciseService } from '../../excercise/application/exercise.service';
 import { User } from '../../user/domain/User.entity';
 import { GetRoutineRequestDto } from '../dto/getRoutine.request.dto';
@@ -15,41 +15,59 @@ import { RoutineResponseDto } from '../dto/routine.response.dto';
 export class RoutineService {
   private readonly logger = new Logger(RoutineService.name);
   constructor(
+    private dataSource: DataSource,
     @InjectRepository(Routine) private readonly routineRepository: Repository<Routine>,
     readonly exerciseService: ExerciseService,
   ) {}
 
   @Transactional()
   async bulkInsertRoutines(user: User, saveRoutines: SaveRoutinesRequestDto) {
-    const newExercises = await this.exerciseService.findNewExercises(saveRoutines);
-    if (newExercises.length > 0) {
-      console.log(newExercises);
-      await this.exerciseService.bulkInsertExercises({ exercises: newExercises });
-    }
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const isExistRoutine = await queryRunner.manager.find(Routine, {
+        where: { name: saveRoutines.routineName, user: { id: user.id } },
+        relations: ['user'],
+        lock: { mode: 'pessimistic_write' },
+      });
+      if (isExistRoutine) {
+        throw new BadRequestException('Routine already exists');
+      }
+      const newExercises = await this.exerciseService.findNewExercises(saveRoutines);
+      if (newExercises.length > 0) {
+        console.log(newExercises);
+        await this.exerciseService.bulkInsertExercises({ exercises: newExercises });
+      }
+      const exerciseEntities = await this.exerciseService.findAll(saveRoutines.exercises);
+      const newRoutines = await Promise.all(
+        saveRoutines.routines.map(async (routine) => {
+          const { routineName, exerciseName, bodyPart } = routine;
 
-    const promiseRoutine = await Promise.all(
-      saveRoutines.routines.map(async (routine) => {
-        const { routineName, exerciseName, bodyPart } = routine;
-
-        const exercise = await this.exerciseService.findByExerciseNameAndBodyPart({ exerciseName, bodyPart });
-        if (!exercise) {
-          throw new NotFoundException(
-            `exercise (${exerciseName}, ${bodyPart}) not found. : ${JSON.stringify(exercise)} `,
+          const exercise = exerciseEntities.find(
+            (entity) => entity.exerciseName === exerciseName && entity.bodyPart === bodyPart,
           );
-        }
-        return new Routine({ name: routineName, user: user, exercise: exercise });
-      }),
-    );
-
-    const savedRoutines = await this.routineRepository.insert(promiseRoutine);
-    const ids: number[] = savedRoutines.identifiers.map((routine) => {
-      return routine.id;
-    });
-    const foundRoutines = await this.routineRepository.find({
-      where: { id: In(ids) },
-      relations: ['user', 'exercise'],
-    });
-    return foundRoutines.map((routine) => new RoutineResponseDto(routine));
+          if (!exercise) {
+            throw new NotFoundException(`exercise (${exerciseName}, ${bodyPart}) can not found. `);
+          }
+          return new Routine({ name: routineName, user: user, exercise: exercise });
+        }),
+      );
+      const insertResult = await this.routineRepository.insert(newRoutines);
+      const ids: number[] = insertResult.identifiers.map((routine) => {
+        return routine.id;
+      });
+      const foundRoutines = await this.routineRepository.find({
+        where: { id: In(ids) },
+        relations: ['user', 'exercise'],
+      });
+      return foundRoutines.map((routine) => new RoutineResponseDto(routine));
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException(err);
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async getRoutineByName(getRoutineRequest: GetRoutineRequestDto, user: User) {
@@ -63,29 +81,50 @@ export class RoutineService {
 
   @Transactional()
   async bulkUpdateRoutines(updateRoutineRequest: UpdateRoutinesRequestDto, user: User) {
-    const NewExercises = await this.exerciseService.findNewExercises(updateRoutineRequest);
-    if (NewExercises.length > 0) {
-      await this.exerciseService.bulkInsertExercises({ exercises: NewExercises });
-    }
-    // Todo: 저장하기 전에 중복 체크 필요....
-    const promiseUpdatedIds = await Promise.all(
-      updateRoutineRequest.updateData.map(async (updateRoutine) => {
-        const { routineName, id, exerciseName, bodyPart } = updateRoutine;
-        console.log(routineName);
-        const exercise = await this.exerciseService.findByExerciseNameAndBodyPart({ exerciseName, bodyPart });
-        if (!exercise) {
-          throw new NotFoundException(`${exerciseName}, ${bodyPart} can not find`);
-        }
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-        await this.routineRepository.update(id, { name: routineName, exercise, user });
-        return id;
-      }),
-    );
-    const foundRoutines = await this.routineRepository.find({
-      where: { id: In(promiseUpdatedIds) },
-      relations: ['exercise', 'user'],
-    });
-    return foundRoutines.map((routine) => new RoutineResponseDto(routine));
+    try {
+      const isExistRoutine = await queryRunner.manager.find(Routine, {
+        where: { name: updateRoutineRequest.routineName, user: { id: user.id } },
+        relations: ['user'],
+        lock: { mode: 'pessimistic_write' },
+      });
+      if (!isExistRoutine) {
+        throw new BadRequestException('Routine is not exists');
+      }
+
+      const NewExercises = await this.exerciseService.findNewExercises(updateRoutineRequest);
+      if (NewExercises.length > 0) {
+        await this.exerciseService.bulkInsertExercises({ exercises: NewExercises });
+      }
+      const exercises = await this.exerciseService.findAll(updateRoutineRequest.exercises);
+      const promiseUpdatedIds = await Promise.all(
+        updateRoutineRequest.updateData.map(async (updateRoutine) => {
+          const { routineName, id, exerciseName, bodyPart } = updateRoutine;
+          const exercise = exercises.find(
+            (exercise) => exercise.exerciseName === exerciseName && exercise.bodyPart === bodyPart,
+          );
+          if (!exercise) {
+            throw new NotFoundException(`${exerciseName}, ${bodyPart} can not find`);
+          }
+
+          await queryRunner.manager.update(Routine, id, { name: routineName, exercise, user });
+          return id;
+        }),
+      );
+      const foundRoutines = await queryRunner.manager.find(Routine, {
+        where: { id: In(promiseUpdatedIds) },
+        relations: ['exercise', 'user'],
+      });
+      return foundRoutines.map((routine) => new RoutineResponseDto(routine));
+    } catch (errors) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException(errors);
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   @Transactional()
