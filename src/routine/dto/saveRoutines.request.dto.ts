9ce1308e5800953d0e196d8a82e272a7fc92aa61
JW0203 <widwoo0203@gmail.com>
2024-07-19 01:21:14 +0900

    feat: add routineName field

--- src/routine/dto/saveRoutines.request.dto.ts
+++ src/routine/dto/saveRoutines.request.dto.ts
@@ -1,9 +1,11 @@
-import { ValidateNested } from 'class-validator';
+import { IsString, ValidateNested } from 'class-validator';
 import { SaveRoutineFormatDto } from './saveRoutine.format.dto';
 import { Type } from 'class-transformer';
 import { ExerciseDataFormatDto } from '../../common/dto/exerciseData.format.dto';
 
 export class SaveRoutinesRequestDto {
+  @IsString()
+  routineName: string;
   @ValidateNested()
   @Type(() => SaveRoutineFormatDto)
   routines: SaveRoutineFormatDto[];
