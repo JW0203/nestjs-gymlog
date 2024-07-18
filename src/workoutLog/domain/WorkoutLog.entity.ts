f07a4537c25267205a403c753e291738174c9ee9
JW0203 <widwoo0203@gmail.com>
2024-07-18 13:12:57 +0900

    refactor: change column names to avoid conflict with reserved words in mysql

--- src/workoutLog/domain/WorkoutLog.entity.ts
+++ src/workoutLog/domain/WorkoutLog.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
 import { Exercise } from '../../excercise/domain/Exercise.entity';
-import { IsInt, IsNotEmpty, Length, validateOrReject } from 'class-validator';
+import { IsInt, IsNotEmpty, Max, Min, validate, validateOrReject } from 'class-validator';
 
 @Entity()
 export class WorkoutLog extends Timestamps {
@@ -11,21 +11,23 @@ export class WorkoutLog extends Timestamps {
 
   @IsNotEmpty()
   @IsInt()
-  @Length(1, 100)
+  @Min(1)
+  @Max(100)
   @Column()
-  set: number;
+  setCount: number;
 
   @IsNotEmpty()
   @IsInt()
-  @Length(1, 1000)
+  @Min(1)
+  @Max(1000)
   @Column()
   weight: number;
 
   @IsNotEmpty()
   @IsInt()
-  @Length(1, 1000)
+  @Min(1)
   @Column()
-  repeat: number;
+  repeatCount: number;
 
   @ManyToOne(() => Exercise, (exercise) => exercise.workoutLogs)
   public exercise: Exercise;
@@ -33,17 +35,24 @@ export class WorkoutLog extends Timestamps {
   @ManyToOne(() => User, (user) => user.workoutLogs)
   public user: User;
 
-  constructor(params: { set: number; weight: number; repeat: number; exercise: Exercise; user: User }) {
+  constructor(params: { setCount: number; weight: number; repeatCount: number; exercise: Exercise; user: User }) {
     super();
     if (params) {
-      this.set = params.set;
+      this.setCount = params.setCount;
       this.weight = params.weight;
-      this.repeat = params.repeat;
+      this.repeatCount = params.repeatCount;
       this.exercise = params.exercise;
       this.user = params.user;
     }
-    validateOrReject(this).catch((errors) => {
-      console.log('(WorkoutLog entity validation failed). Errors: ', errors);
+    // validateOrReject(this).catch((errors) => {
+    //   console.log('(WorkoutLog entity validation failed). Errors: ', errors);
+    // });
+    validate(this).then((errors) => {
+      if (errors.length > 0) {
+        console.log('(WorkoutLog entity validation failed). errors:', errors);
+      } else {
+        console.log('(WorkoutLog entity validation succeed)');
+      }
     });
   }
 }
