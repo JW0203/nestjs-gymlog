5748329e6dcc29219b66a3e2639fae7226414792
JW0203 <widwoo0203@gmail.com>
2024-07-26 21:22:21 +0900

    refactor: use transactional decoration

--- src/workoutLog/application/workoutLog.service.ts
+++ src/workoutLog/application/workoutLog.service.ts
@@ -1,80 +1,69 @@
 import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WorkoutLog } from '../domain/WorkoutLog.entity';
-import { DataSource, In, Raw, Repository } from 'typeorm';
+import { In, Raw, Repository } from 'typeorm';
 import { ExerciseService } from '../../excercise/application/exercise.service';
-import { IsolationLevel, Transactional } from 'typeorm-transactional';
+import { Transactional } from 'typeorm-transactional';
 import { UserService } from '../../user/application/user.service';
 import { SoftDeleteWorkoutLogRequestDto } from '../dto/softDeleteWorkoutLog.request.dto';
 import { User } from '../../user/domain/User.entity';
 import { SaveWorkoutLogsRequestDto } from '../dto/saveWorkoutLogs.request.dto';
 import { UpdateWorkoutLogsRequestDto } from '../dto/updateWorkoutLogs.request.dto';
 import { WorkoutLogResponseDto } from '../dto/workoutLog.response.dto';
-import { UpdateWorkoutLogsResponseDto } from '../dto/updateWorkoutLogs.response.dto';
 
 @Injectable()
 export class WorkoutLogService {
   private readonly logger = new Logger(WorkoutLogService.name);
   constructor(
-    private dataSource: DataSource,
     @InjectRepository(WorkoutLog)
     private workoutLogRepository: Repository<WorkoutLog>,
     private readonly exerciseService: ExerciseService,
     private readonly userService: UserService,
   ) {}
 
+  @Transactional()
   async bulkInsertWorkoutLogs(userId: number, saveWorkoutLogs: SaveWorkoutLogsRequestDto): Promise<any> {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction(IsolationLevel.SERIALIZABLE);
-    try {
-      const user = await queryRunner.manager.findOne(User, { where: { id: userId } });
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
-      const newExercises = await this.exerciseService.findNewExercises(saveWorkoutLogs);
-      if (newExercises.length > 0) {
-        await this.exerciseService.bulkInsertExercises({ exercises: newExercises });
-      }
-
-      const exerciseEntities = await this.exerciseService.findAll(saveWorkoutLogs.exercises);
-
-      this.logger.log('Make data for bulk insert');
-      const workoutLogs = await Promise.all(
-        saveWorkoutLogs.workoutLogs.map(async (workoutLog) => {
-          const { exerciseName, bodyPart, ...workoutLogData } = workoutLog;
-
-          const exercise = exerciseEntities.find(
-            (exercise) => exercise.exerciseName === exerciseName && exercise.bodyPart === bodyPart,
-          );
-          if (!exercise) {
-            throw new NotFoundException('Exercise not found');
-          }
-          return new WorkoutLog({
-            setCount: workoutLogData.setCount,
-            weight: workoutLogData.weight,
-            repeatCount: workoutLogData.repeatCount,
-            exercise,
-            user,
-          });
-        }),
-      );
-
-      const result = await this.workoutLogRepository.insert(workoutLogs);
-      const ids = result.identifiers.map((id) => id.id);
-
-      const savedWorkoutLogs = await this.workoutLogRepository.find({
-        where: { id: In(ids) },
-        relations: ['user', 'exercise'],
-      });
-      await queryRunner.commitTransaction();
-      return savedWorkoutLogs.map((workoutLog) => new WorkoutLogResponseDto(workoutLog));
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      throw new BadRequestException(error);
-    } finally {
-      await queryRunner.release();
+    const user = await this.userService.findOneById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    const newExercises = await this.exerciseService.findNewExercises(saveWorkoutLogs);
+    if (newExercises.length > 0) {
+      await this.exerciseService.bulkInsertExercises({ exercises: newExercises });
+    }
+
+    const exerciseEntities = await this.exerciseService.findAll(saveWorkoutLogs.exercises);
+
+    const workoutLogs = await Promise.all(
+      saveWorkoutLogs.workoutLogs.map(async (workoutLog) => {
+        const { exerciseName, bodyPart, setCount, weight, repeatCount } = workoutLog;
+
+        const exercise = exerciseEntities.find(
+          (exercise) => exercise.exerciseName === exerciseName && exercise.bodyPart === bodyPart,
+        );
+        if (!exercise) {
+          throw new NotFoundException('Exercise not found');
+        }
+
+        const newWorkoutLog = new WorkoutLog({
+          setCount,
+          weight,
+          repeatCount,
+          exercise,
+          user,
+        });
+        return newWorkoutLog;
+      }),
+    );
+
+    const insertedResults = await this.workoutLogRepository.insert(workoutLogs);
+    const ids = insertedResults.identifiers.map((result) => result.id);
+
+    const savedWorkoutLogs = await this.workoutLogRepository.find({
+      where: { id: In(ids) },
+      relations: ['user', 'exercise'],
+    });
+    return savedWorkoutLogs.map((workoutLog) => new WorkoutLogResponseDto(workoutLog));
   }
 
   async getWorkoutLogsByDay(date: string, userId: number) {
