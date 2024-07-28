5480c8f3b6467b843bd944f92353d508623e33e1
JW0203 <widwoo0203@gmail.com>
2024-07-29 00:45:08 +0900

    feat: add update function

--- src/routine/domain/Routine.entity.ts
+++ src/routine/domain/Routine.entity.ts
@@ -3,6 +3,7 @@ import { Timestamps } from '../../TimeStamp.entity';
 import { User } from '../../user/domain/User.entity';
 import { validateOrReject } from 'class-validator';
 import { Exercise } from '../../excercise/domain/Exercise.entity';
+import { Logger } from '@nestjs/common';
 
 @Entity()
 export class Routine extends Timestamps {
@@ -29,5 +30,17 @@ export class Routine extends Timestamps {
         console.log('(Routine entity validation failed). Errors: ', errors);
       });
     }
+  }
+
+  update(params: { name: string; user: User; exercise: Exercise }) {
+    this.name = params.name;
+    this.user = params.user;
+    this.exercise = params.exercise;
+
+    validateOrReject(this).catch((errors) => {
+      const logger = new Logger('Routine Entity Update');
+      logger.error('Validation failed during update.', errors);
+      throw errors;
+    });
   }
 }
