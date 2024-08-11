6b90129a16c127f0a0b58e0fc616101fbac116d7
JW0203 <widwoo0203@gmail.com>
2024-08-11 18:19:42 +0900

    feat: make lock type of mysql

---
+++ src/common/const/inject.constant.ts
@@ -0,0 +1,4 @@
+import { TypeORMLockMode } from '../../excercise/infrastructure/typeormExercise.repository';
+
+export const EXERCISE_REPOSITORY = 'EXERCISE_REPOSITORY';
+export type LockMode = TypeORMLockMode;
