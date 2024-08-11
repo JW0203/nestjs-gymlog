0c83cfb26f18c16a2b9c5eb7c6276f8a54351d0a
JW0203 <widwoo0203@gmail.com>
2024-08-11 18:22:40 +0900

    refactor: remove unecessary code

--- src/common/const/inject.constant.ts
+++ src/common/const/inject.constant.ts
@@ -1,4 +1,1 @@
-import { TypeORMLockMode } from '../../excercise/infrastructure/typeormExercise.repository';
-
 export const EXERCISE_REPOSITORY = 'EXERCISE_REPOSITORY';
-export type LockMode = TypeORMLockMode;
