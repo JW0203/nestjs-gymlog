189cf9d7bb74fa3ada74009f8431f421aeaa6aa9
JW0203 <widwoo0203@gmail.com>
2024-08-14 23:30:50 +0900

    refactor: apply GetExercisesRequestDto

--- src/excercise/domain/exercise.repository.ts
+++ src/excercise/domain/exercise.repository.ts
@@ -4,10 +4,11 @@ import { ExerciseDataFormatDto } from '../../common/dto/exerciseData.format.dto'
 import { SaveExercisesRequestDto } from '../dto/saveExercises.request.dto';
 import { ExerciseDataResponseDto } from '../../common/dto/exerciseData.response.dto';
 import { DeleteExerciseRequestDto } from '../dto/deleteExercise.request.dto';
+import { GetExercisesRequestDto } from '../dto/getExercises.request.dto';
 
 export interface ExerciseRepository {
   findOneByExerciseNameAndBodyPart(exerciseName: string, bodyPart: BodyPart): Promise<Exercise | null>;
-  findExercisesByExerciseNameAndBodyPart(exercisesData: ExerciseDataFormatDto[], lock?: boolean): Promise<Exercise[]>;
+  findExercisesByExerciseNameAndBodyPart(getExercisesRequest: GetExercisesRequestDto): Promise<Exercise[]>;
   findAll(): Promise<Exercise[]>;
   findNewExercises(exerciseDataArray: SaveExercisesRequestDto): Promise<ExerciseDataResponseDto[]>;
   bulkInsertExercises(exerciseDataArray: SaveExercisesRequestDto): Promise<ExerciseDataResponseDto[]>;
