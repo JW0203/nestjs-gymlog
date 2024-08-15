8bf36d596ebe34569694dc9dd0d9c390e32eb6df
JW0203 <widwoo0203@gmail.com>
2024-08-15 15:58:03 +0900

    feat: add user repository in providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './presentation/user.controller';
 import { UserService } from './application/user.service';
 import { AuthModule } from '../auth/application/auth.module';
+import { USER_REPOSITORY } from '../common/const/inject.constant';
+import { TypeormUserRepository } from './infrastructure/typeormUser.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, { provide: USER_REPOSITORY, useClass: TypeormUserRepository }],
   exports: [UserService],
 })
 export class UserModule {}
