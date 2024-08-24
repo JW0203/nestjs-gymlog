1b235e97fa240bc43e58338620847ff6a084928b
JW0203 <widwoo0203@gmail.com>
2024-08-24 23:52:22 +0900

    feat: add and remove method

--- src/routine/domain/routine.repository.ts
+++ src/routine/domain/routine.repository.ts
@@ -3,8 +3,9 @@ import { Routine } from './Routine.entity';
 
 export interface RoutineRepository {
   bulkInsertRoutines(newRoutines: Routine[]): Promise<Routine[]>;
-  findOneRoutineByName(name: string, user: User): Promise<Routine | null>;
   findRoutinesByName(name: string, user: User): Promise<Routine[]>;
+  findOneRoutineById(id: number, user: User): Promise<Routine | null>;
+  findRoutinesByIds(ids: number[], user: User): Promise<Routine[]>;
   bulkUpdateRoutines(updateRoutines: Routine[]): Promise<Routine[]>;
   softDeleteRoutines(routineIds: number[]): Promise<void>;
   findRoutineNameByUserIdLockMode(routineName: string, user: User): Promise<Routine[]>;
