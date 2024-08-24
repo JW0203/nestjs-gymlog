61b08ea849a04a5cdf5faa8d72347add4da5a9bc
JW0203 <widwoo0203@gmail.com>
2024-08-24 13:22:00 +0900

    refactor: change method names

--- src/routine/domain/routine.repository.ts
+++ src/routine/domain/routine.repository.ts
@@ -3,8 +3,8 @@ import { Routine } from './Routine.entity';
 
 export interface RoutineRepository {
   bulkInsertRoutines(newRoutines: Routine[]): Promise<Routine[]>;
-  getOneRoutineByName(name: string, user: User): Promise<Routine | null>;
-  getRoutinesByName(name: string, user: User): Promise<Routine[]>;
+  findOneRoutineByName(name: string, user: User): Promise<Routine | null>;
+  findRoutinesByName(name: string, user: User): Promise<Routine[]>;
   bulkUpdateRoutines(updateRoutines: Routine[]): Promise<Routine[]>;
   softDeleteRoutines(routineIds: number[]): Promise<void>;
   findRoutineNameByUserIdLockMode(routineName: string, user: User): Promise<Routine[]>;
