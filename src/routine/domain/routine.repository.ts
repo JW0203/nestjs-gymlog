27aafb41d7d06a246c668ea861f0ffb0a9913143
JW0203 <widwoo0203@gmail.com>
2024-08-24 12:59:32 +0900

    feat: change prameters and add methods

--- src/routine/domain/routine.repository.ts
+++ src/routine/domain/routine.repository.ts
@@ -1,13 +1,11 @@
 import { User } from '../../user/domain/User.entity';
-import { SaveRoutinesRequestDto } from '../dto/saveRoutines.request.dto';
-import { RoutineResponseDto } from '../dto/routine.response.dto';
-import { GetRoutineRequestDto } from '../dto/getRoutine.request.dto';
-import { UpdateRoutinesRequestDto } from '../dto/updateRoutines.request.dto';
-import { DeleteRoutineRequestDto } from '../dto/deleteRoutine.request.dto';
+import { Routine } from './Routine.entity';
 
 export interface RoutineRepository {
-  bulkInsertRoutines(user: User, saveRoutines: SaveRoutinesRequestDto): Promise<RoutineResponseDto[]>;
-  getRoutineByName(getRoutineRequest: GetRoutineRequestDto, user: User): Promise<RoutineResponseDto[]>;
-  bulkUpdateRoutines(updateRoutineRequest: UpdateRoutinesRequestDto, user: User): Promise<RoutineResponseDto[]>;
-  softDeleteRoutines(deleteRoutineRequestDto: DeleteRoutineRequestDto, user: User): Promise<any>;
+  bulkInsertRoutines(newRoutines: Routine[]): Promise<Routine[]>;
+  getOneRoutineByName(name: string, user: User): Promise<Routine | null>;
+  getRoutinesByName(name: string, user: User): Promise<Routine[]>;
+  bulkUpdateRoutines(updateRoutines: Routine[]): Promise<Routine[]>;
+  softDeleteRoutines(routineIds: number[]): Promise<void>;
+  findRoutineNameByUserIdLockMode(routineName: string, user: User): Promise<Routine[]>;
 }
