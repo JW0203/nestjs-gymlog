34d773bf71bb19104aa8370a69b4f271613a8e93
JW0203 <widwoo0203@gmail.com>
2024-08-27 13:07:13 +0900

    feat: make test for user api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import { sign } from 'jsonwebtoken';
+import { DataSource } from 'typeorm';
 
-describe('AppController (e2e)', () => {
+function generateTestToken(): string {
+  const payload = { id: 2 };
+  return sign(payload, process.env.JWT_SECRET || 'mykey', { expiresIn: '1h' });
+}
+
+describe('e2e test', () => {
   let app: INestApplication;
+  let token: string;
+  let dataSource: DataSource;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    initializeTransactionalContext();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
+    dataSource = moduleFixture.get<DataSource>(DataSource);
+    await dataSource.dropDatabase();
+    await dataSource.synchronize();
+    token = generateTestToken();
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/health').expect(200).expect('Success Health Check');
+  describe('AppController', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer()).get('/').expect(200).expect('Success Health Check!!');
+    });
+  });
+
+  describe('User', () => {
+    it('user sign up', () => {
+      return request(app.getHttpServer())
+        .post('/users/')
+        .send({
+          email: 'test1@email.com',
+          password: '12345678',
+          name: 'Tester',
+        })
+        .expect(201);
+    });
+  });
+
+  it('user sign in', () => {
+    return request(app.getHttpServer())
+      .get('/users/')
+      .send({
+        email: 'test1@email.com',
+        password: '12345678',
+      })
+      .expect(200);
+  });
+
+  it('my information', () => {
+    return request(app.getHttpServer()).get('/users/my/').set('Authorization', `Bearer ${token}`).expect(200);
+  });
+
+  it('Delete my account', () => {
+    return request(app.getHttpServer()).delete('/users/').set('Authorization', `Bearer ${token}`).expect(204);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
