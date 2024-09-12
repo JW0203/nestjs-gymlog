59e0eca18272f7b49058e658b89f884fd134b48d
Joey <widwoo0203@gmail.com>
2024-09-12 10:02:31 -0700

    도커 기반으로 배포하기 수정완료 ( issue #71) (#76)
    ECR 리포지토리 이름 수점

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import { DataSource } from 'typeorm';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_NAME'),
+        synchronize: true,
         logging: true,
         namingStrategy: new SnakeNamingStrategy(),
       }),
