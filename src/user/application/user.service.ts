4764c3310b740a71f20d163af39092cea174c443
JW0203 <widwoo0203@gmail.com>
2024-10-17 10:59:49 -0700

    feat: test make new User

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -3,6 +3,9 @@ import { USER_REPOSITORY } from '../../common/const/inject.constant';
 import { UserRepository } from '../domain/user.repository';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from '../../auth/application/auth.service';
+import { SignUpRequestDto } from '../dto/signUp.request.dto';
+import { Transactional } from 'typeorm-transactional';
+import { User } from '../domain/User.entity';
 
 @Injectable()
 export class UserService {
@@ -11,4 +14,10 @@ export class UserService {
     private readonly configService: ConfigService,
     private readonly authService: AuthService,
   ) {}
+
+  @Transactional()
+  async signUp(signUpRequestDto: SignUpRequestDto): Promise<any> {
+    const newUserEntity = new User(signUpRequestDto);
+    await this.userRepository.signUp(newUserEntity);
+  }
 }
