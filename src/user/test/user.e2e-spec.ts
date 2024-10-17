64bbc9e413a0837b1087bd54bc4e69374e24f21f
JW0203 <widwoo0203@gmail.com>
2024-10-17 14:11:40 -0700

    feat: test for checking duplicated user email

--- src/user/test/user.e2e-spec.ts
+++ src/user/test/user.e2e-spec.ts
@@ -9,7 +9,7 @@ describe('User API (e2e)', () => {
   let app: INestApplication;
   let dataSource: DataSource;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     initializeTransactionalContext();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -27,6 +27,16 @@ describe('User API (e2e)', () => {
     const signUpRequestDto = { email: 'newuser@email.com', password: '12345678', name: 'tester' };
     const response = await request(app.getHttpServer()).post('/users').send(signUpRequestDto);
     expect(response.status).toBe(201);
+  });
+
+  it('가입된 이 메일로 회원 가입 요청이 주어지면 409 Conflict 코드를 반환한다.', async () => {
+    await request(app.getHttpServer())
+      .post('/users')
+      .send({ email: 'test@email.com', password: '12345678', name: 'existing tester' });
+
+    const signUpRequestDto = { email: 'test@email.com', password: '12345678', name: 'new tester' };
+    const response = await request(app.getHttpServer()).post('/users').send(signUpRequestDto);
+    expect(response.status).toBe(409);
   });
 
   afterAll(async () => {
