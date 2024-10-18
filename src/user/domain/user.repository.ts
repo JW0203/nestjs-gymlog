e90b7fd376cf4d5f996501d1ffd5ef768e21f920
JW0203 <widwoo0203@gmail.com>
2024-10-17 17:10:55 -0700

    feat: make findOneUserByEmail repository

--- src/user/domain/user.repository.ts
+++ src/user/domain/user.repository.ts
@@ -3,4 +3,5 @@ import { User } from './User.entity';
 export interface UserRepository {
   signUp(newUserEntity: User): Promise<User>;
   findOneUserByEmailLockMode(email: string): Promise<User | null>;
+  findOneUserByEmail(email: string): Promise<User | null>;
 }
