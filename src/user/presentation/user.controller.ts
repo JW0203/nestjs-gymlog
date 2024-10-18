9b57bb43b8963114ff56ee9b058aae57a90866af
JW0203 <widwoo0203@gmail.com>
2024-10-17 17:12:19 -0700

    feat: make sign in controller

--- src/user/presentation/user.controller.ts
+++ src/user/presentation/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { UserService } from '../application/user.service';
 import { SignUpRequestDto } from '../dto/signUp.request.dto';
+import { SignInRequestDto } from '../dto/signIn.request.dto';
 
 @Controller('users')
 export class UserController {
@@ -10,5 +11,11 @@ export class UserController {
   @HttpCode(201)
   signUp(@Body() signUpRequestDto: SignUpRequestDto) {
     return this.userService.signUp(signUpRequestDto);
+  }
+
+  @Post('sign-in')
+  @HttpCode(200)
+  signIn(@Body() signInRequestDto: SignInRequestDto) {
+    return this.userService.signIn(signInRequestDto);
   }
 }
