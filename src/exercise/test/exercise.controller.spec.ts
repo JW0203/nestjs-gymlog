4ede2def665b5f984170f703dad464b0addf3e97
JW0203 <widwoo0203@gmail.com>
2024-12-02 22:54:53 +0900

    refactor: remove rejects test code, add decoration test code

--- src/exercise/test/exercise.controller.spec.ts
+++ src/exercise/test/exercise.controller.spec.ts
@@ -4,12 +4,19 @@ import { ExerciseService } from '../application/exercise.service';
 import { DeleteExerciseRequestDto } from '../dto/deleteExercise.request.dto';
 import { SaveExercisesRequestDto } from '../dto/saveExercises.request.dto';
 import { ExerciseDataFormatDto } from '../../common/dto/exerciseData.format.dto';
-import { BadRequestException, ConflictException } from '@nestjs/common';
+import { RequestMethod } from '@nestjs/common';
 import { BodyPart } from '../../common/bodyPart.enum';
+import { HTTP_CODE_METADATA, METHOD_METADATA, PATH_METADATA } from '@nestjs/common/constants';
 
+const mockExerciseService = {
+  bulkSoftDelete: jest.fn(),
+  bulkInsertExercises: jest.fn(),
+  findOneByExerciseNameAndBodyPart: jest.fn(),
+  findAll: jest.fn(),
+};
 describe('ExerciseController', () => {
   let exerciseController: ExerciseController;
-  let exerciseService: ExerciseService;
+  let exerciseService: jest.Mocked<typeof mockExerciseService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,18 +24,13 @@ describe('ExerciseController', () => {
       providers: [
         {
           provide: ExerciseService,
-          useValue: {
-            bulkSoftDelete: jest.fn(),
-            bulkInsertExercises: jest.fn(),
-            findOneByExerciseNameAndBodyPart: jest.fn(),
-            findAll: jest.fn(),
-          },
+          useValue: mockExerciseService,
         },
       ],
     }).compile();
 
     exerciseController = module.get<ExerciseController>(ExerciseController);
-    exerciseService = module.get<ExerciseService>(ExerciseService);
+    exerciseService = module.get(ExerciseService);
   });
 
   describe('softDelete', () => {
@@ -38,12 +40,16 @@ describe('ExerciseController', () => {
       expect(exerciseService.bulkSoftDelete).toHaveBeenCalledWith(deleteExerciseRequestDto);
     });
 
-    it('should throw BadRequestException if some exercises do not exist', async () => {
-      const deleteExerciseRequestDto: DeleteExerciseRequestDto = { ids: [1, 2, 3] };
-      jest
-        .spyOn(exerciseService, 'bulkSoftDelete')
-        .mockRejectedValue(new BadRequestException('Some exercises do not exist'));
-      await expect(exerciseController.softDelete(deleteExerciseRequestDto)).rejects.toThrow(BadRequestException);
+    it('should have correct Http status code 204', () => {
+      const httpCode = Reflect.getMetadata(HTTP_CODE_METADATA, exerciseController.softDelete);
+      expect(httpCode).toBe(204);
+    });
+
+    it('should have correct method DELETE and path "/" ', () => {
+      const path = Reflect.getMetadata(PATH_METADATA, exerciseController.softDelete);
+      const method = Reflect.getMetadata(METHOD_METADATA, exerciseController.softDelete);
+      expect(method).toBe(RequestMethod.DELETE);
+      expect(path).toBe('/');
     });
   });
 
@@ -56,14 +62,16 @@ describe('ExerciseController', () => {
       expect(exerciseService.bulkInsertExercises).toHaveBeenCalledWith(saveExercisesRequestDto);
     });
 
-    it('should throw ConflictException if some exercises already exist', async () => {
-      const saveExercisesRequestDto: SaveExercisesRequestDto = {
-        exercises: [{ exerciseName: 'Push Up', bodyPart: BodyPart.CHEST }],
-      };
-      jest
-        .spyOn(exerciseService, 'bulkInsertExercises')
-        .mockRejectedValue(new ConflictException('Some or all exercises already exist'));
-      await expect(exerciseController.saveExercises(saveExercisesRequestDto)).rejects.toThrow(ConflictException);
+    it('should have correct Http status code 201', () => {
+      const httpCode = Reflect.getMetadata(HTTP_CODE_METADATA, exerciseController.saveExercises);
+      expect(httpCode).toBe(201);
+    });
+
+    it('should have correct method POST and path "/" ', () => {
+      const path = Reflect.getMetadata(PATH_METADATA, exerciseController.saveExercises);
+      const method = Reflect.getMetadata(METHOD_METADATA, exerciseController.saveExercises);
+      expect(method).toBe(RequestMethod.POST);
+      expect(path).toBe('/');
     });
   });
 
@@ -74,12 +82,16 @@ describe('ExerciseController', () => {
       expect(exerciseService.findOneByExerciseNameAndBodyPart).toHaveBeenCalledWith(exerciseData);
     });
 
-    it('should throw NotFoundException if exercise is not found', async () => {
-      const exerciseData: ExerciseDataFormatDto = { exerciseName: 'Push Up', bodyPart: BodyPart.CHEST };
-      jest
-        .spyOn(exerciseService, 'findOneByExerciseNameAndBodyPart')
-        .mockRejectedValue(new BadRequestException('Exercise not found'));
-      await expect(exerciseController.getExerciseByNameAndBodyPart(exerciseData)).rejects.toThrow(BadRequestException);
+    it('should have correct Http status code 200', () => {
+      const httpCode = Reflect.getMetadata(HTTP_CODE_METADATA, exerciseController.getExerciseByNameAndBodyPart);
+      expect(httpCode).toBe(200);
+    });
+
+    it('should have correct method GET and path "/" ', () => {
+      const path = Reflect.getMetadata(PATH_METADATA, exerciseController.getExerciseByNameAndBodyPart);
+      const method = Reflect.getMetadata(METHOD_METADATA, exerciseController.getExerciseByNameAndBodyPart);
+      expect(method).toBe(RequestMethod.GET);
+      expect(path).toBe('/');
     });
   });
 
@@ -87,6 +99,18 @@ describe('ExerciseController', () => {
     it('should call findAll', async () => {
       await exerciseController.getAll();
       expect(exerciseService.findAll).toHaveBeenCalled();
+    });
+
+    it('should have correct Http status code 200', () => {
+      const httpCode = Reflect.getMetadata(HTTP_CODE_METADATA, exerciseController.getAll);
+      expect(httpCode).toBe(200);
+    });
+
+    it('should have correct method GET and path "all" ', () => {
+      const path = Reflect.getMetadata(PATH_METADATA, exerciseController.getAll);
+      const method = Reflect.getMetadata(METHOD_METADATA, exerciseController.getAll);
+      expect(method).toBe(RequestMethod.GET);
+      expect(path).toBe('all');
     });
   });
 });
