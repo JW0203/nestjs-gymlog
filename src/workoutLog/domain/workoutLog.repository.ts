f39f36e8d16fa18ba46b83d87b4a2c1275d7ee8a
JW0203 <widwoo0203@gmail.com>
2024-12-07 16:26:22 +0900

    refactor: rename a file name workout -> workoutLog

--- src/workoutLog/domain/workoutLog.repository.ts
+++ src/workoutLog/domain/workoutLog.repository.ts
@@ -1,7 +1,7 @@
 import { User } from '../../user/domain/User.entity';
 import { WorkoutLog } from './WorkoutLog.entity';
 
-export interface WorkoutRepository {
+export interface WorkoutLogRepository {
   bulkInsertWorkoutLogs(newWorkoutLogs: WorkoutLog[]): Promise<WorkoutLog[]>;
   bulkUpdateWorkoutLogs(UpdateWorkoutLogs: WorkoutLog[]): Promise<WorkoutLog[]>;
   findWorkoutLogsByDay(date: string, userId: number): Promise<WorkoutLog[]>;
