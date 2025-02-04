a632390414760715e7da67ed885245f428508cdf
JW0203 <widwoo0203@gmail.com>
2025-02-04 15:47:43 +0900

    feat: add new method to find workoutLogs to aggregate records

--- src/workoutLog/domain/workoutLog.repository.ts
+++ src/workoutLog/domain/workoutLog.repository.ts
@@ -1,5 +1,8 @@
 import { User } from '../../user/domain/User.entity';
 import { WorkoutLog } from './WorkoutLog.entity';
+import { BestWorkoutLog } from '../dto/findBestWorkoutLogs.response.dto';
+import { FindWorkoutLogsByYearMonthResponseDto } from '../dto/findWorkoutLogsByYearMonth.response.dto';
+import { FindWorkoutLogsByYearResponseDto } from '../dto/findWorkoutLogsByYear.response.dto';
 
 export interface WorkoutLogRepository {
   bulkInsertWorkoutLogs(newWorkoutLogs: WorkoutLog[]): Promise<WorkoutLog[]>;
@@ -8,4 +11,7 @@ export interface WorkoutLogRepository {
   softDeleteWorkoutLogs(ids: number[], user: User): Promise<void>;
   findWorkoutLogsByUser(user: User): Promise<WorkoutLog[]>;
   findWorkoutLogsByIdsLockMode(ids: number[], userId: number): Promise<WorkoutLog[]>;
+  findWorkoutLogsByYear(user: User, year: string): Promise<FindWorkoutLogsByYearResponseDto[]>;
+  findWorkoutLogsByYearMonth(user: User, year: string, month: string): Promise<FindWorkoutLogsByYearMonthResponseDto[]>;
+  findBestWorkoutLogs(): Promise<BestWorkoutLog[]>;
 }
