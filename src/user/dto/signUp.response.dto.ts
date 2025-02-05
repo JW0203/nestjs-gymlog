d432986c0f7cc5989e22d9c8455cf984ba2978bd
JW0203 <widwoo0203@gmail.com>
2025-02-06 02:31:46 +0900

    refactor: rename 'name' to 'nickName'

--- src/user/dto/signUp.response.dto.ts
+++ src/user/dto/signUp.response.dto.ts
@@ -12,13 +12,13 @@ export class SignUpResponseDto {
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(15)
-  @Matches(/^[A-Za-z0-9]+$/)
-  name: string;
+  @Matches(/^[a-zA-Z\uAC00-\uD7A3][a-zA-Z0-9\uAC00-\uD7A3]*$/) //문자는 영어나 한글로 시작하고 공백을 허용하지 않는다.,
+  nickName: string;
 
-  constructor(params: { id: number; email: string; name: string }) {
+  constructor(params: { id: number; email: string; nickName: string }) {
     this.id = params.id;
     this.email = params.email;
-    this.name = params.name;
+    this.nickName = params.nickName;
 
     validateOrReject(this).catch((error) => console.log('SignUpResponseDto validation failed', error));
   }
