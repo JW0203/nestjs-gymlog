8f45c61c8c3495fafd0c9107b1490a88ef1029e5
JW0203 <widwoo0203@gmail.com>
2025-02-07 23:33:53 +0900

    refactor: remove exercise subscriber and add workout log module

--- src/exercise/excercise.module.ts
+++ src/exercise/excercise.module.ts
@@ -1,21 +1,16 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Exercise } from './domain/Exercise.entity';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ExerciseService } from './application/exercise.service';
 import { ExerciseController } from './presentation/exercise.controller';
 import { LoggerModule } from '../common/Logger/logger.module';
 import { EXERCISE_REPOSITORY } from '../common/const/inject.constant';
 import { TypeOrmExerciseRepository } from './infrastructure/typeormExercise.repository';
-import { ExerciseSubscriber } from './infrastructure/typeormExercise.subscriber';
-import { WorkoutLog } from '../workoutLog/domain/WorkoutLog.entity';
+import { WorkoutLogModule } from '../workoutLog/workoutLog.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exercise, LoggerModule, WorkoutLog])],
-  providers: [
-    ExerciseService,
-    ExerciseSubscriber,
-    { provide: EXERCISE_REPOSITORY, useClass: TypeOrmExerciseRepository },
-  ],
+  imports: [TypeOrmModule.forFeature([Exercise]), LoggerModule, forwardRef(() => WorkoutLogModule)],
+  providers: [ExerciseService, { provide: EXERCISE_REPOSITORY, useClass: TypeOrmExerciseRepository }],
   controllers: [ExerciseController],
   exports: [ExerciseService],
 })
