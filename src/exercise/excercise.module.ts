58fe2eaeb890d8c2ad8131d6872f7b7758e54dde
JW0203 <widwoo0203@gmail.com>
2025-02-12 23:40:52 +0900

    feat: add MaxWeightPerExerciseModule

--- src/exercise/excercise.module.ts
+++ src/exercise/excercise.module.ts
@@ -6,10 +6,10 @@ import { ExerciseController } from './presentation/exercise.controller';
 import { LoggerModule } from '../common/Logger/logger.module';
 import { EXERCISE_REPOSITORY } from '../common/const/inject.constant';
 import { TypeOrmExerciseRepository } from './infrastructure/typeormExercise.repository';
-import { WorkoutLogModule } from '../workoutLog/workoutLog.module';
+import { MaxWeightPerExerciseModule } from '../maxWeightPerExercise/maxWeightPerExercise.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exercise]), LoggerModule, forwardRef(() => WorkoutLogModule)],
+  imports: [TypeOrmModule.forFeature([Exercise]), LoggerModule, forwardRef(() => MaxWeightPerExerciseModule)],
   providers: [ExerciseService, { provide: EXERCISE_REPOSITORY, useClass: TypeOrmExerciseRepository }],
   controllers: [ExerciseController],
   exports: [ExerciseService],
