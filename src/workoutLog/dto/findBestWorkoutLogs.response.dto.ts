2f258ba0b9169bd8a4e16b730aa75f028b4d4390
JW0203 <widwoo0203@gmail.com>
2025-02-12 23:28:13 +0900

    feat: make dto as reponse type of find best workout log

--- src/workoutLog/dto/findBestWorkoutLogs.response.dto.ts
+++ src/workoutLog/dto/findBestWorkoutLogs.response.dto.ts
@@ -1,16 +1,28 @@
 import { BodyPart } from '../../common/bodyPart.enum';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, Matches, Max, MaxLength, Min, MinLength } from 'class-validator';
+import { IsExerciseName } from '../../common/validation/isExerciseName.validation';
 
 export class BestWorkoutLog {
   @IsNotEmpty()
-  @IsString()
+  @IsExerciseName()
   exerciseName: string;
+
   @IsNotEmpty()
-  @IsString()
-  userEmail: string;
+  @MinLength(2)
+  @MaxLength(15)
+  @Matches(/^[a-zA-Z\uAC00-\uD7A3][a-zA-Z0-9\uAC00-\uD7A3]*$/) //문자는 영어나 한글로 시작하고 공백을 허용하지 않는다.,
+  userNickName: string;
+
   @IsNotEmpty()
+  @IsEnum(BodyPart)
   bodyPart: BodyPart;
+
   @IsNotEmpty()
   @IsNumber()
-  weight: number;
+  @Min(0)
+  @Max(1000)
+  maxWeight: number;
+
+  @IsDate()
+  achieveDate: Date;
 }
