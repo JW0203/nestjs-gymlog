f59cb3e85d5e045aeb72b808b53dacb8bab22c34
JW0203 <widwoo0203@gmail.com>
2025-02-16 17:29:10 +0900

    refactor: remove unsued import UserModule and export routineService

--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './domain/Routine.entity';
 import { RoutineService } from './application/routine.service';
 import { ExerciseModule } from '../exercise/excercise.module';
-import { UserModule } from '../user/user.module';
 import { RoutineController } from './presentation/routine.controller';
 import { LoggerModule } from '../common/Logger/logger.module';
 import { TypeormRoutineRepository } from './infrastructure/typeormRoutine.repository';
 import { ROUTINE_REPOSITORY } from '../common/const/inject.constant';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Routine]), ExerciseModule, UserModule, LoggerModule],
+  imports: [TypeOrmModule.forFeature([Routine]), ExerciseModule, LoggerModule],
   providers: [RoutineService, { provide: ROUTINE_REPOSITORY, useClass: TypeormRoutineRepository }],
   controllers: [RoutineController],
+  exports: [RoutineService],
 })
 export class RoutineModule {}
