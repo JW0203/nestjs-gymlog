5ab68288083272178d7779ed9e043453c3b86c68
JW0203 <widwoo0203@gmail.com>
2025-03-14 01:04:19 +0900

    refactor: change from BcryptHasher to BcryptHasherService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './presentation/user.controller';
 import { UserService } from './application/user.service';
 import { AuthModule } from '../auth/application/auth.module';
-import { PASSWORD_HASHER, USER_REPOSITORY } from '../common/const/inject.constant';
+import { USER_REPOSITORY } from '../common/const/inject.constant';
 import { TypeormUserRepository } from './infrastructure/typeormUser.repository';
-import { BycptHasher } from './application/bcryptHasher.service';
+import { BcryptHasherService } from './application/bcryptHasher.service';
 import { WorkoutLogModule } from '../workoutLog/workoutLog.module';
 import { RoutineModule } from '../routine/routine.module';
 
@@ -18,11 +18,7 @@ import { RoutineModule } from '../routine/routine.module';
     forwardRef(() => RoutineModule),
   ],
   controllers: [UserController],
-  providers: [
-    UserService,
-    { provide: USER_REPOSITORY, useClass: TypeormUserRepository },
-    { provide: PASSWORD_HASHER, useClass: BycptHasher },
-  ],
+  providers: [UserService, BcryptHasherService, { provide: USER_REPOSITORY, useClass: TypeormUserRepository }],
   exports: [UserService],
 })
 export class UserModule {}
