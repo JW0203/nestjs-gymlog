bc17fc56ecd56b48853459f06c4dfb90874cd634
JW0203 <widwoo0203@gmail.com>
2025-05-22 18:08:15 +0900

    feat: implement the saveRoutineExercises service

---
+++ src/routineExercise/application/routineExercise.service.ts
@@ -0,0 +1,66 @@
+import { ROUTINE_EXERCISE_REPOSITORY } from '../../common/const/inject.constant';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { RoutineExerciseRepository } from '../domain/routineExercise.repository';
+import { SaveRoutineExerciseRequestDto } from '../dto/saveRoutineExercise.request.dto';
+import { RoutineExercise } from '../domain/RoutineExercise.entity';
+import { ExerciseService } from '../../exercise/application/exercise.service';
+import { SaveRoutineExerciseResponseDto } from '../dto/saveRoutineExercise.response.dto';
+
+@Injectable()
+export class RoutineExerciseService {
+  constructor(
+    @Inject(ROUTINE_EXERCISE_REPOSITORY)
+    private readonly routineExerciseRepository: RoutineExerciseRepository,
+
+    readonly exerciseService: ExerciseService,
+  ) {}
+
+  async saveRoutineExercises(
+    saveRoutineExerciseRequestDto: SaveRoutineExerciseRequestDto[],
+  ): Promise<SaveRoutineExerciseResponseDto[]> {
+    const receivedDataArray: RoutineExercise[] = saveRoutineExerciseRequestDto.map((data) => {
+      return new RoutineExercise({
+        order: data.order,
+        routine: data.routine,
+        exercise: data.exercise,
+      });
+    });
+    const exercises = saveRoutineExerciseRequestDto.map(({ exercise }) => exercise);
+    const newExercises = await this.exerciseService.findNewExercises({ exercises });
+    if (newExercises.length > 0) {
+      await this.exerciseService.bulkInsertExercises({ exercises: newExercises });
+    }
+
+    const exerciseEntities = await this.exerciseService.findExercisesByExerciseNameAndBodyPart(exercises);
+
+    const saveDataArray: RoutineExercise[] = receivedDataArray.map((data) => {
+      const exerciseName = data.exercise.exerciseName;
+      const exerciseBodyPart = data.exercise.bodyPart;
+      const exercise = exerciseEntities.find(
+        (entity) => entity.exerciseName === exerciseName && entity.bodyPart === exerciseBodyPart,
+      );
+      if (!exercise) {
+        throw new NotFoundException(`exercise (${exerciseName}, ${exerciseBodyPart}) can not found. `);
+      }
+
+      return new RoutineExercise({
+        order: data.order,
+        routine: data.routine,
+        exercise: exercise,
+      });
+    });
+
+    const savedData = await this.routineExerciseRepository.saveRoutineExercises(saveDataArray);
+    return savedData.map((data) => {
+      return {
+        id: data.id,
+        order: data.order,
+        routineId: data.routine.id,
+        routineName: data.routine.name,
+        exerciseId: data.exercise.id,
+        exerciseName: data.exercise.exerciseName,
+        exerciseBodyPart: data.exercise.bodyPart,
+      };
+    });
+  }
+}
