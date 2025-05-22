7d647c43ae935bd3687c7c1efdf04ec717346ba3
JW0203 <widwoo0203@gmail.com>
2025-05-22 21:40:05 +0900

    refactor: add all entities

--- test/utils/getMySql.TypeOrm.config.ts
+++ test/utils/getMySql.TypeOrm.config.ts
@@ -1,13 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { WorkoutLog } from '../../src/workoutLog/domain/WorkoutLog.entity';
+import { Exercise } from '../../src/exercise/domain/Exercise.entity';
+import { Routine } from '../../src/routine/domain/Routine.entity';
+import { User } from '../../src/user/domain/User.entity';
+import { RoutineExercise } from '../../src/routineExercise/domain/RoutineExercise.entity';
 
-export const getMySqlTypeOrmConfig = (entities: Function[]): TypeOrmModuleOptions => ({
+export const getMySqlTypeOrmConfig = (): TypeOrmModuleOptions => ({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
   password: 'password',
   database: 'gymlog_test',
-  entities: entities,
+  entities: [User, Routine, Exercise, WorkoutLog, RoutineExercise],
   synchronize: true,
   logging: false,
 });
