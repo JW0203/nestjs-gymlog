79b54834253463ad45b191390a78d3a58f68a290
JW0203 <widwoo0203@gmail.com>
2025-05-22 23:57:30 +0900

    feat: function for creating and saving routine to do repository test

---
+++ test/utils/createTestRoutine.repo.layer.ts
@@ -0,0 +1,14 @@
+import { User } from '../../src/user/domain/User.entity';
+import { DataSource } from 'typeorm';
+import { Routine } from '../../src/routine/domain/Routine.entity';
+
+export async function createTestRoutineRepo(dataSource: DataSource, user: User, overrides: Partial<Routine> = {}) {
+  const routineRepository = dataSource.getRepository(Routine);
+  const routineData = {
+    user,
+    name: 'testRoutine',
+    ...overrides,
+  };
+  const newRoutine = routineRepository.create(routineData);
+  return await routineRepository.save(newRoutine);
+}
