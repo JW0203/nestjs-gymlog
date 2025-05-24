a1b2c9ef93fd6d46a50df8cdab0a09f8196225f8
JW0203 <widwoo0203@gmail.com>
2025-05-24 22:47:56 +0900

    feat: add RoutineExerciseItemDto for FindDataByRoutineIdResponseDto

--- src/routineExercise/dto/fineDataByRoutineId.response.dto.ts
+++ src/routineExercise/dto/fineDataByRoutineId.response.dto.ts
@@ -1,6 +1,38 @@
 import { BodyPart } from '../../common/bodyPart.enum';
 
-export class FineDataByRoutineIdResponseDto {
+export class FindDataByRoutineIdResponseDto {
   routineName: string;
-  routines: { order: number; exerciseName: string; bodyPart: BodyPart }[];
+  routines: RoutineExerciseItemDto[];
+
+  constructor(params: { routineName: string; routines: RoutineExerciseItemDto[] }) {
+    this.routineName = params.routineName;
+    this.routines = params.routines;
+  }
+}
+
+export class RoutineExerciseItemDto {
+  id: number;
+  order: number;
+  exerciseName: string;
+  bodyPart: BodyPart;
+
+  constructor(params: { id: number; order: number; exerciseName: string; bodyPart: BodyPart }) {
+    this.id = params.id;
+    this.order = params.order;
+    this.exerciseName = params.exerciseName;
+    this.bodyPart = params.bodyPart;
+  }
+
+  static fromEntity(data: {
+    id: number;
+    order: number;
+    exercise: { exerciseName: string; bodyPart: BodyPart };
+  }): RoutineExerciseItemDto {
+    return new RoutineExerciseItemDto({
+      id: data.id,
+      order: data.order,
+      exerciseName: data.exercise.exerciseName,
+      bodyPart: data.exercise.bodyPart,
+    });
+  }
 }
