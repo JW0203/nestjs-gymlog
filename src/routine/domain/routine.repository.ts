832d46d49e0fb870df14cec73812789064674592
JW0203 <widwoo0203@gmail.com>
2025-05-26 23:06:16 +0900

    feat: add findRoutinesByIds repository

--- src/routine/domain/routine.repository.ts
+++ src/routine/domain/routine.repository.ts
@@ -4,8 +4,9 @@ import { Routine } from './Routine.entity';
 export interface RoutineRepository {
   findOneRoutineByName(name: string, user: User): Promise<Routine | null>;
   findOneRoutineById(id: number, user: User): Promise<Routine | null>;
+  findRoutinesByIds(ids: number[], user: User): Promise<Routine[]>;
   updateRoutine(updateRoutines: Routine): Promise<Routine>;
-  softDeleteRoutine(routineId: number): Promise<void>;
+  softDeleteRoutines(routineIds: number[]): Promise<void>;
   findAllByUserId(userId: number): Promise<Routine[]>;
   saveRoutine(newRoutine: Routine): Promise<Routine>;
 }
