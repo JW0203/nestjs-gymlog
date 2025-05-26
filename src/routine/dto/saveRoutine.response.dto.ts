6362d51400ad63574debf17ab0da3c666d1c357e
JW0203 <widwoo0203@gmail.com>
2025-05-26 23:07:44 +0900

    feat: add static to make datatype using given data

--- src/routine/dto/saveRoutine.response.dto.ts
+++ src/routine/dto/saveRoutine.response.dto.ts
@@ -1,15 +1,31 @@
-export class SaveRoutineResponseDto {
-  id: number;
-  name: string;
-  userId: number;
-  createdAt: string;
-  updatedAt: string;
+import { ExerciseInRoutineDto } from '../../routineExercise/dto/exerciseInRoutine.dto';
+import { RoutineExercise } from '../../routineExercise/domain/RoutineExercise.entity';
 
-  constructor(routine: any) {
-    this.id = routine.id;
-    this.name = routine.name;
-    this.userId = routine.user.id;
-    this.createdAt = routine.createdAt;
-    this.updatedAt = routine.updatedAt;
+export class SaveRoutineResponseDto {
+  routineId: number;
+  routineName: string;
+  routines: ExerciseInRoutineDto[];
+
+  constructor(params: { routineId: number; routineName: string; routines: ExerciseInRoutineDto[] }) {
+    if (params) {
+      this.routineId = params.routineId;
+      this.routineName = params.routineName;
+      this.routines = params.routines;
+    }
+  }
+
+  static fromEntities(entities: RoutineExercise[]): SaveRoutineResponseDto {
+    if (entities.length === 0) {
+      throw new Error('RoutineExercise list is empty.');
+    }
+
+    const { routine } = entities[0];
+    const routines = entities.map((item) => ExerciseInRoutineDto.fromData(item));
+
+    return new SaveRoutineResponseDto({
+      routineId: routine.id,
+      routineName: routine.name,
+      routines,
+    });
   }
 }
