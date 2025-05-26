8cb2d711fbf3b826e79c98e611f6517a9c66fe16
JW0203 <widwoo0203@gmail.com>
2025-05-26 12:16:10 +0900

    refactor: change the datatype

--- src/routine/dto/getAllRoutineByUser.response.dto.ts
+++ src/routine/dto/getAllRoutineByUser.response.dto.ts
@@ -1,19 +1,13 @@
-import { BodyPart } from '../../common/bodyPart.enum';
+import { Routine } from '../domain/Routine.entity';
 
 export class GetAllRoutineByUserResponseDto {
   id: number;
   name: string;
-  exerciseId: number;
-  bodyPart: BodyPart;
-  exerciseName: string;
   userId: number;
 
-  constructor(routine: any) {
+  constructor(routine: Routine) {
     this.id = routine.id;
     this.name = routine.name;
-    this.exerciseId = routine.exercise.id;
-    this.bodyPart = routine.exercise.bodyPart;
-    this.exerciseName = routine.exercise.exerciseName;
     this.userId = routine.user.id;
   }
 }
