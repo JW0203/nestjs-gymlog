406a12808448aa7a873c3446384068199a4ba3cf
JW0203 <widwoo0203@gmail.com>
2025-05-28 00:24:13 +0900

    fix: update test code to reflect changes in response and saving logic

--- src/routine/test/routine.repository.spec.ts
+++ src/routine/test/routine.repository.spec.ts
@@ -1,7 +1,6 @@
 import { RoutineRepository } from '../domain/routine.repository';
 import { Routine } from '../domain/Routine.entity';
 import { Exercise } from '../../exercise/domain/Exercise.entity';
-import { BodyPart } from '../../common/bodyPart.enum';
 import { User } from '../../user/domain/User.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ROUTINE_REPOSITORY } from '../../common/const/inject.constant';
@@ -9,11 +8,9 @@ import { TypeormRoutineRepository } from '../infrastructure/typeormRoutine.repos
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource, In } from 'typeorm';
 import { getMySqlTypeOrmConfig } from '../../../test/utils/getMySql.TypeOrm.config';
-import { ExerciseDataFormatDto } from '../../common/dto/exerciseData.format.dto';
 import { UpdateRoutine } from '../dto/updateRoutine.dto';
-import { createTestUserRepo } from '../../../test/utils/createTestUser.repo.layer';
-import { createTestRoutineRepo } from '../../../test/utils/createTestRoutine.repo.layer';
-import { createTestExerciseRepo } from '../../../test/utils/createTestExercise.repo.layer';
+import { createAndSaveTestUserRepo } from '../../../test/utils/createAndSaveTestUser.repo.layer';
+import { createAndSaveTestRoutineRepo } from '../../../test/utils/createAndSaveTestRoutine.repo.layer';
 
 describe('Test RoutineRepository', () => {
   let routineRepository: RoutineRepository;
@@ -41,12 +38,18 @@ describe('Test RoutineRepository', () => {
       await dataSource.synchronize();
     });
     it('should save a routine', async () => {
-      const user = await createTestUserRepo(dataSource);
+      const user = await createAndSaveTestUserRepo(dataSource);
       user.id = 1;
       const routineName = 'testRoutine';
       const newRoutine = new Routine({ name: routineName, user });
 
       const result = await routineRepository.saveRoutine(newRoutine);
+      const queryResult = await dataSource.getRepository(Routine).findOne({
+        where: { id: result.id },
+        relations: ['user'],
+      });
+
+      expect(result).toEqual(queryResult);
       expect(result.user.id).toBe(user.id);
       expect(result.name).toBe(routineName);
       expect(result.id).toBe(1);
@@ -60,7 +63,7 @@ describe('Test RoutineRepository', () => {
     });
 
     it('should return an empty array when a user searches for a non-existent routine name', async () => {
-      const user: User = await createTestUserRepo(dataSource);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
       const notExistRoutineName: string = 'back-routine';
       const result = await routineRepository.findOneRoutineByName(notExistRoutineName, user);
 
@@ -68,39 +71,45 @@ describe('Test RoutineRepository', () => {
     });
 
     it('should find routines saved by the user when a user searches for a routine name', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-      const savedRoutine = await createTestRoutineRepo(dataSource, user);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      const routine = await createAndSaveTestRoutineRepo(dataSource, user);
 
-      const result = await routineRepository.findOneRoutineByName(savedRoutine.name, user);
+      const result = await routineRepository.findOneRoutineByName(routine.name, user);
+      const queryResult = await dataSource.getRepository(Routine).findOne({
+        where: { name: routine.name },
+        relations: ['user'],
+      });
 
       expect(result).not.toBeNull();
-      expect(result?.name).toBe(savedRoutine.name);
+      expect(result).toEqual(queryResult);
+      expect(result?.name).toBe(routine.name);
       expect(result?.user.id).toBe(user.id);
     });
   });
 
-  describe('findOneRoutinesById', () => {
+  describe('findOneRoutineById', () => {
     beforeEach(async () => {
       await dataSource.dropDatabase();
       await dataSource.synchronize();
     });
 
     it('should find a routine saved by a user when a user searches for a routine id', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-      const routines = await createTestRoutineRepo(dataSource, user);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      const routine = await createAndSaveTestRoutineRepo(dataSource, user);
 
-      await routineRepository.saveRoutine(routines);
+      await routineRepository.saveRoutine(routine);
 
       const result = await routineRepository.findOneRoutineById(1, user);
       const findOneQueryResult = await dataSource
         .getRepository(Routine)
         .findOne({ where: { id: 1, user: { id: 1 } }, relations: ['user'] });
 
+      expect(result).not.toBeNull();
       expect(result).toStrictEqual(findOneQueryResult);
     });
 
     it('should return null when a user searches for a non-existent routine id', async () => {
-      const user: User = await createTestUserRepo(dataSource);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
 
       const result = await routineRepository.findOneRoutineById(999, user);
       const findOneQueryResult = await dataSource
@@ -111,7 +120,6 @@ describe('Test RoutineRepository', () => {
     });
   });
 
-  /*
   describe('updateRoutine', () => {
     beforeEach(async () => {
       await dataSource.dropDatabase();
@@ -119,60 +127,14 @@ describe('Test RoutineRepository', () => {
     });
 
     it('should return an updated routine when a user update a routine', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-      // const exerciseInfo: ExerciseDataFormatDto[] = [
-      //   { exerciseName: 'Push-up', bodyPart: BodyPart.CHEST },
-      //   { exerciseName: 'Pull-up', bodyPart: BodyPart.BACK },
-      //   { exerciseName: '', bodyPart: BodyPart.LEGS },
-      // ];
-      // const Exercises = await createTestExerciseRepo(dataSource);
-      const Routines = await createTestRoutineRepo(dataSource, user);
-      await routineRepository.saveRoutine(Routines);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      const routine = await createAndSaveTestRoutineRepo(dataSource, user);
+      routine.name = 'updated Routine';
 
-      const existenceRoutines = await dataSource
-        .getRepository(Routine)
-        .find({ where: { id: 1, user: { id: 1 } }, relations: ['user'] });
+      const result = await routineRepository.updateRoutine(routine);
 
-      const givenUpdateDataArray: UpdateRoutine[] = [
-        {
-          order: 1,
-          exerciseName: 'deadlift',
-          bodyPart: BodyPart.LEGS,
-        },
-        {
-          order: 2,
-          exerciseName: 'goblet squat',
-          bodyPart: BodyPart.LEGS,
-        },
-        {
-          order: 3,
-          exerciseName: 'barbell sumo squat',
-          bodyPart: BodyPart.LEGS,
-        },
-      ];
-
-      const updatedRoutines = await Promise.all(
-        existenceRoutines.map(async (routine: Routine, i) => {
-          const updateData = givenUpdateDataArray[i];
-          const { order, exerciseName, bodyPart } = updateData;
-          const exercise = await createExercise(dataSource, { exerciseName, bodyPart });
-          if (id === routine.id) {
-            routine.update({
-              name: routineName,
-              exercise,
-              user,
-            });
-          }
-          return routine;
-        }),
-      );
-
-      const result = await routineRepository.updateRoutine(updatedRoutines);
-
-      const findQueryResult = await dataSource
-        .getRepository(Routine)
-        .find({ where: { id: In([1, 2, 3]) }, relations: ['user', 'exercise'] });
-      expect(result).toStrictEqual(findQueryResult);
+      expect(result.id).toBe(routine.id);
+      expect(result.name).toBe(routine.name);
     });
   });
 
@@ -183,73 +145,72 @@ describe('Test RoutineRepository', () => {
     });
 
     it('should soft delete routines when user delete routines with their ids', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-      const exerciseInfo: ExerciseDataFormatDto[] = [
-        { exerciseName: 'Push-up', bodyPart: BodyPart.CHEST },
-        { exerciseName: 'Pull-up', bodyPart: BodyPart.BACK },
-      ];
-      const Exercises = await createExercises(dataSource, exerciseInfo);
-      const Routines: Routine = createTestRoutineRepo(dataSource, user);
-      await routineRepository.saveRoutine(Routines);
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      await createAndSaveTestRoutineRepo(dataSource, user);
 
-      const result = await routineRepository.softDeleteRoutines([1, 2]);
-      const findQueryResult = await dataSource.getRepository(Routine).find({ where: { id: In([1, 2]) } });
+      const result = await routineRepository.softDeleteRoutines([1]);
+
+      const findQueryResult = await routineRepository.findOneRoutineById(1, user);
       const findOneWithDeletedQueryResult = await dataSource
         .getRepository(Routine)
-        .find({ where: { id: In([1, 2]) }, withDeleted: true });
+        .findOne({ where: { id: 1 }, withDeleted: true });
 
       expect(result).toBe(undefined);
-      expect(findQueryResult.length).toBe(0);
-      expect(findOneWithDeletedQueryResult.length).toBe(2);
+      expect(findQueryResult).toBeNull();
       expect(findOneWithDeletedQueryResult).not.toBeNull();
+      expect(findOneWithDeletedQueryResult?.id).toBe(1);
     });
   });
 
   describe('findAllByUserId', () => {
     beforeEach(async () => {
-      const queryRunner = dataSource.createQueryRunner();
-      await queryRunner.connect();
-
-      await queryRunner.query('DELETE FROM routine');
-      await queryRunner.query('DELETE FROM user');
-      await queryRunner.query('DELETE FROM exercise');
-
-      await queryRunner.release();
+      await dataSource.dropDatabase();
+      await dataSource.synchronize();
     });
 
-    it('should return all routines saved by a user when a user find their routines with their id', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-      const exerciseInfo: ExerciseDataFormatDto[] = [
-        { exerciseName: 'Push-up', bodyPart: BodyPart.CHEST },
-        { exerciseName: 'Pull-up', bodyPart: BodyPart.BACK },
-      ];
-      const Exercises = await createTestExerciseRepo(dataSource);
-      const Routines = await createTestRoutineRepo(dataSource, user);
-      await routineRepository.saveRoutine(Routines);
+    it('should return all routines when a user find routines by user ID', async () => {
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      await createAndSaveTestRoutineRepo(dataSource, user);
+      await createAndSaveTestRoutineRepo(dataSource, user, { name: 'testRoutine2' });
 
       const result = await routineRepository.findAllByUserId(user.id);
       const findQueryResult = await dataSource
         .getRepository(Routine)
-        .find({ where: { user: { id: user.id } }, relations: ['user', 'exercise'] });
+        .find({ where: { user: { id: user.id } }, relations: ['user'] });
 
-      expect(result).toStrictEqual(findQueryResult);
-    });
-
-    it('should return all routines save by a user when a user find using findAllByUserId', async () => {
-      const user: User = await createTestUserRepo(dataSource);
-
-      const result = await routineRepository.findAllByUserId(user.id);
-      const findQueryResult = await dataSource
-        .getRepository(Routine)
-        .find({ where: { user: { id: user.id } }, relations: ['user', 'exercise'] });
-
-      expect(result).toStrictEqual(findQueryResult);
-    });
-
-    afterAll(async () => {
-      await dataSource.destroy();
+      expect(result).toEqual(findQueryResult);
+      expect(result[0].name).toBe('testRoutine');
+      expect(result[1].name).toBe('testRoutine2');
     });
   });
 
- */
+  describe('findRoutinesByIds', () => {
+    beforeEach(async () => {
+      await dataSource.dropDatabase();
+      await dataSource.synchronize();
+    });
+
+    it('should return routines when a user requests them by routine IDs', async () => {
+      const user: User = await createAndSaveTestUserRepo(dataSource);
+      await createAndSaveTestRoutineRepo(dataSource, user);
+      await createAndSaveTestRoutineRepo(dataSource, user, { name: 'testRoutine2' });
+      await createAndSaveTestRoutineRepo(dataSource, user, { name: 'testRoutine3' });
+
+      const result = await routineRepository.findRoutinesByIds([1, 2], user);
+      const findQueryResult = await dataSource
+        .getRepository(Routine)
+        .find({ where: { id: In([1, 2]), user: { id: user.id } }, relations: ['user'] });
+
+      expect(result).toEqual(findQueryResult);
+      expect(result[0].user.id).toBe(1);
+      expect(result[0].id).toBe(1);
+      expect(result[0].name).toBe('testRoutine');
+      expect(result[1].id).toBe(2);
+      expect(result[1].name).toBe('testRoutine2');
+    });
+  });
+
+  afterAll(async () => {
+    await dataSource.destroy();
+  });
 });
